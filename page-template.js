f39f1081c5ec0d6934e843c30340201306a56233
jasmin3ashl3y <jasmine@ashleyit.com>
2021-11-02 21:40:05 -0400

    separate page-template file from app.js for cleaner code

--- page-template.js
+++ page-template.js
@@ -1,4 +1,5 @@
-
+const generatePage = (name, github) => {
+    return `
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -10,8 +11,11 @@
         </head>
         
         <body>
-            <h1>jazzy</h1>
-            <h2><a href="https://github.com/jasmin3ashl3y">Github</a></h2>
+            <h1>${name}</h1>
+            <h2><a href="https://github.com/${github}">Github</a></h2>
         </body>
         </html>
-        
+        `;
+}
+
+module.exports = generatePage;
